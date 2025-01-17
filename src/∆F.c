a021c011bb9a26e3374e299db4bdfcc24700b9d2
Peter M Siegel <petermsiegel@gmail.com>
2025-01-16 22:02:13 -0800

    Cleaning up restoration...

--- src/∆F.c
+++ src/∆F.c
@@ -6,12 +6,12 @@
        cc -O3 -dynamiclib -o ∆F.dylib ∆F.c
    Returns:  rc outBuf outPLen.  APL code does out← outPLen↑out
    rc=¯1:   output buffer not big enough for transformed fString.
-            The output buffer is not examined (and may contain junk).
+            In this case, the output buffer is not examined (and may contain junk).
             *outPLen=0;
    rc> 0:   an error occurred. Return APL error number (e.g. 11: DOMAIN ERROR).
             The error string is in outBuf, with length *outPLen.
    rc= 0:   all is well. 
-            The transformed fString is in outBuf with length *outPLen.
+            The output buffer, outBuf, contains the transformed fString with length *outPLen.
    Note: We don't end strings with 0 for <fString> or <outBuf>. 
          As a Dyalog APL char vector, <fString> may validly contain 0 or any unicode char.
 */
@@ -35,8 +35,8 @@
 #  include <stdlib.h>  // for alloca
 #endif 
 
-#define CHAR4  uint32_t  
-#define CHAR2  uint16_t
+#define WIDE4  uint32_t  
+#define WIDE2  uint16_t
 #define  INT4   int32_t 
 #define BLANK_STR    u" "  // A string, not a char. const.
 //        Join pseudo-primitive
@@ -97,28 +97,28 @@
         if (expandSq){   \
         /* Slower path. Possible expansion of single quotes (APL rule) */ \
             for(ix=0; ix<len; (grp##Len)++, ix++){\
-                grp##Buf[grp##Len]= (CHAR4) str[ix];\
+                grp##Buf[grp##Len]= (WIDE4) str[ix];\
                 if (grp##Buf[grp##Len] == SQ) {\
                     if (grp##Len+1 >= grp##Max) ERROR_SPACE;\
-                    grp##Buf[++(grp##Len)]= (CHAR4) SQ;\
+                    grp##Buf[++(grp##Len)]= (WIDE4) SQ;\
                 }\
             }\
         } else{\
          /* Faster path. Copy as is. */ \
             for(ix=0; ix<len; ){\
-                  grp##Buf[(grp##Len)++]= (CHAR4) str[ix++];\
+                  grp##Buf[(grp##Len)++]= (WIDE4) str[ix++];\
             }\
         }\
 }
 #define ADDCH_GENERIC(ch, grp) {\
       if (grp##Len+1 >= grp##Max) ERROR_SPACE;\
-      grp##Buf[(grp##Len)++]= (CHAR4) ch;\
+      grp##Buf[(grp##Len)++]= (WIDE4) ch;\
 }
 
 /* OUTPUT BUFFER MANAGEMENT ROUTINES */
 #define OutBuf(str, len)    ADDBUF_GENERIC(str, len, out, 0)
 #define OutBufSq(str, len)  ADDBUF_GENERIC(str, len, out, 1)
-#define OutStr(str)         OutBuf(str, Str2Len((CHAR2 *) str))
+#define OutStr(str)         OutBuf(str, Wide2Len((WIDE2 *) str))
 #define OutCh(ch)           ADDCH_GENERIC(ch, out)
 
 /* END OUTPUT BUFFER MANAGEMENT ROUTINES */
@@ -128,7 +128,7 @@
      CodeOut
 */
 #define CodeInit             codeLen=0
-#define CodeStr(str)         ADDBUF_GENERIC(str, Str2Len((CHAR2 *)str), code, 0)  
+#define CodeStr(str)         ADDBUF_GENERIC(str, Wide2Len((WIDE2 *)str), code, 0)  
 #define CodeCh(ch)           ADDCH_GENERIC(ch, code)
 #define CodeOut              {OutBuf(codeBuf, codeLen); CodeInit;} 
 
@@ -145,18 +145,18 @@
     }\
     snprintf(nstr, CODENUM_MAXDIG, "%d", tnum);\
     for (i=0;  i<CODENUM_MAXDIG && nstr[i]; ++i){\
-        CodeCh((CHAR2)nstr[i]);\
+        CodeCh((WIDE2)nstr[i]);\
     }\
 }
 /* End Handle Special code buffer */                  
 
 #define STRLEN_MAX  512  
-// Str2Len(str)
-//   <str> is a null-terminated CHAR2 string.
+// Wide2Len(str)
+//   <str> is a null-terminated WIDE2 string.
 //   Returns the length of the string, sans the final null.
 //   If there is no final null, we will either abnormally terminate or 
 //   return a length of STRLEN_MAX.
-static inline int Str2Len(CHAR2 *str) {
+static inline int Wide2Len(WIDE2 *str) {
     int len;
     for (len=0; len<STRLEN_MAX && str[len]; ++len)
         ;
@@ -190,7 +190,7 @@ static inline int Str2Len(CHAR2 *str) {
 #define STATE(new)  { oldState=state; state=new;}
 // End STATE MANAGEMENT 
 
-static inline INT4 afterBlanks(CHAR4 fString[], INT4 fStringLen, int inPos){
+static inline INT4 afterBlanks(WIDE4 fString[], INT4 fStringLen, int inPos){
     for (; inPos < fStringLen && SP == fString[inPos]; ++inPos)
            ;
     if (inPos>=fStringLen) 
@@ -213,9 +213,10 @@ static inline INT4 afterBlanks(CHAR4 fString[], INT4 fStringLen, int inPos){
             OutCh(SQ);\
       }\
       OutStr(marker);\
-      m = Str2Len(marker);\
+      m = Wide2Len(marker);\
       for (i=inPos+1; fString[i] == SP; ++i){\
-          if (--m <= 0) OutCh(SP);\
+          if (--m <= 0)\ 
+             OutCh(SP);\
       }\
       OutCh(QT); OutCh(SP);\
       OutStr(code);\
@@ -240,15 +241,15 @@ static inline INT4 afterBlanks(CHAR4 fString[], INT4 fStringLen, int inPos){
        --inPos;
 
 
-int fs_format(const char opts[4], const CHAR4 escCh, 
-              CHAR4 fString[],    INT4 fStringLen, 
-              CHAR4 outBuf[],     INT4 *outPLen){ 
+int fs_format(const char opts[4], const WIDE4 escCh, 
+              WIDE4 fString[],    INT4 fStringLen, 
+              WIDE4 outBuf[],     INT4 *outPLen){ 
    INT4 outMax = *outPLen;          // User must pass in *outPLen as outBuf[outMax]  
    int mode=  opts[0];              // See modes (MODE_...) above
    int debug= opts[1];              // debug (boolean) 
    int useNs= opts[2];              // If 1, pass an anon ns to each Code Fn.           
    int extLib=opts[3];              // If 0, pseudo-primitives are defined internally.
-   CHAR4 crOut= debug? CRVIS: CR;
+   WIDE4 crOut= debug? CRVIS: CR;
 
   int outLen = 0;                        // output buffer length/position; passed back to APL.
   INT4 codeLen= 0;                       // length/position in code buffer. Like outLen.
@@ -263,20 +264,20 @@ int fs_format(const char opts[4], const CHAR4 escCh,
 //    self-doc CF. If so, we output the doc literal text and append the processed CF code.
   const INT4 codeMax = outMax;
 #if USE_ALLOCA
-  CHAR4 *codeBuf = alloca( codeMax * sizeof(CHAR4));  // Automatically freed...
+  WIDE4 *codeBuf = alloca( codeMax * sizeof(WIDE4));  // Automatically freed...
 #else 
-  CHAR4 *codeBuf = malloc( codeMax * sizeof(CHAR4));  // Manually freed...
+  WIDE4 *codeBuf = malloc( codeMax * sizeof(WIDE4));  // Manually freed...
 #endif
 
 // Code sequences...
-CHAR2 *mergeCd = extLib? MERGECD_EXT: MERGECD_INT;
-CHAR2 *aboveCd = extLib? ABOVECD_EXT: ABOVECD_INT;
-CHAR2 *boxCd  =  extLib? BOXCD_EXT:   BOXCD_INT;
-CHAR2 *fmtCd  =  FMTCD_INT;
-CHAR2 *dispCd =  extLib? DISPCD_EXT:  DISPCD_INT;
+WIDE2 *mergeCd = extLib? MERGECD_EXT: MERGECD_INT;
+WIDE2 *aboveCd = extLib? ABOVECD_EXT: ABOVECD_INT;
+WIDE2 *boxCd  =  extLib? BOXCD_EXT:   BOXCD_INT;
+WIDE2 *fmtCd  =  FMTCD_INT;
+WIDE2 *dispCd =  extLib? DISPCD_EXT:  DISPCD_INT;
 // Markers for self-doc code 
-CHAR2 *mergeMarker  = FANCY_MARKERS? u"▶": u"→"; 
-CHAR2 *aboveMarker  = FANCY_MARKERS? u"▼": u"↓";
+WIDE2 *mergeMarker  = FANCY_MARKERS? u"▶": u"→"; 
+WIDE2 *aboveMarker  = FANCY_MARKERS? u"▼": u"↓";
 
 // Preamble code string...
   OutCh(LBR); 
@@ -348,7 +349,7 @@ CHAR2 *aboveMarker  = FANCY_MARKERS? u"▼": u"↓";
       }  
       if (state == TF) {       // Text field 
           if (CUR == escCh){   // Check for escape chars
-            CHAR4 ch= PEEK; 
+            WIDE4 ch= PEEK; 
             ++inPos;
             if (ch == escCh){
                 OutCh(escCh);
